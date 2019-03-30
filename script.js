1c64c1581d12ccf264b15278572c91423dbee352
cioaonk <42707320+cioaonk@users.noreply.github.com>
2019-03-30 05:56:14 -0600

    script file

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function () {
+    "use strict";
+    $(".lobby").click(function () {
+        $("leadShow").hide();
+    });
+});
+
+
+/* if rules clicked: 
+hide play, hide leaderboard, hide home*/
+/*if play clicked:
+hide home, hide rules, hide leaderboard */
+/* if leaderboard clicked:
+hide home, hide rules, hide play*/
